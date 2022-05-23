7cb6460029bd6c1391cf88d7913de8de98d4cfb5
Wilson Christian <59436377+wilson-christian@users.noreply.github.com>
2022-05-23 18:59:17 +0530

    Demo code. Contains some import error, which needs to be fixed.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import Intents
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,22 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    donateIntent()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func donateIntent() {
+        let intent = CreateMomentIntentIntent()
+        intent.suggestedInvocationPhrase = "Create Moment"
+        let interaction = INInteraction(intent: intent, response: nil)
+        interaction.donate { (error) in
+            if error != nil {
+                if let error = error as NSError? {
+                    print("Interaction donation failed: \(error.description)")
+                } else {
+                    print("Successfully donated interaction")
+                }
+            }
+        }
+    }
 }
